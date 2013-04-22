b1fbf6b3c21684ba0dbe1c816fec511c0a4e3da3
solacefarstar <chris.mcmorran@yahoo.com>
2013-04-22 02:54:42 +0000

    Create TREBasj.java

---
+++ TREBasj.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trebash;
+
+/**
+ *
+ * @author Solace
+ */
+public class TREBash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        main main = new main();
+        main.setVisible(true);
+    }
+}
